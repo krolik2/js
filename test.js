e3a0aa8d4095c5bd855887b09c513706a8644b62
Mateusz Krolikowski <krolikmat@gmail.com>
2018-10-22 22:27:09 +0200

    date object done

--- test.js
+++ test.js
@@ -1,19 +1,18 @@
- 
-var Car = function(maxSpeed, driver){
+var birthday = new Date (1985, 0, 15, 11, 15, 25);
+var birthday2 = new Date (1985, 0, 15, 11, 15, 25);
 
-    this.maxSpeed = maxSpeed;
-    this.driver = driver;
-    this.drive = function(speed, time){
-        console.log(speed * time);
-    };
-    this.logDriver = function(){
-        console.log("driver name is " + this.driver);
-    };
+console.log(birthday.getMonth());
+console.log(birthday.getFullYear());
+console.log(birthday.getDate());
+console.log(birthday.getDay());
+console.log(birthday.getHours());
+console.log(birthday.getTime());
+
+if(birthday.getTime() == birthday2.getTime()){
+
+    console.log("birthdays are equal");
+
+} else{
+
+    console.log("birthdays are not equal")
 }
-var myCar = new Car(70, "Ninja Man");
-var myCar2 = new Car(40, "Humpty Dumpty");
-var myCar3 = new Car(10, "Shaun");
-var myCar4 = new Car(90, "James Bond");
-
-myCar.drive(30,5);
-myCar4.logDriver();
