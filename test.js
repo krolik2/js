beca4b3f3c08a73ea919b22aa8d93191b663fcd1
Mateusz Krolikowski <krolikmat@gmail.com>
2018-10-22 21:04:40 +0200

    objects done

--- test.js
+++ test.js
@@ -1,4 +1,23 @@
-var string1 = "abc"
-var string2 = "abC"
+ var myArray = new Array();
+ myArray[0] = 8;
+ myArray[1] = "hello";
 
-console.log(string1 > string2);
+ var myCar = new Object();
+ myCar.maxSpeed = 50;
+ myCar.driver = "Mateusz";
+ myCar.drive = function(){console.log("now driving");};
+
+ myCar.drive();
+
+ var myCar2 = {
+
+     maxSpeed: 70,
+     driver: "Net Ninja",
+     drive: function(speed, time){
+          console.log(speed * time);
+        }
+        
+    };
+ 
+ console.log(myCar2.maxSpeed);
+ myCar2.drive(50, 3);
